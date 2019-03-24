00a8cb3c722d13789a8e14f1359e8653f57ac960
Eric <ericwej@gmail.com>
2019-03-24 13:52:24 -0400

    added js dom

---
+++ js/game.js
@@ -0,0 +1,31 @@
+var boundx = 400;
+var boundy = 300;
+var size = 81;
+
+var x = Math.round(boundx/size);
+var y = Math.round(boundy/size);
+
+function square(node) {
+  var e = document.createElement('input');
+
+  e.setAttribute("class","square");
+  e.setAttribute("pattern","[0-9]{1,2}");
+  e.style.width = x;
+  e.style.height = y;
+  node.appendChild(e);
+  return e;
+}
+
+function makeSquares() {
+  //
+  var parentNode = document.getElementbyId("board");
+
+  for(var i=0; i<boundx; i+=x) {
+    for(var j=0; j<boundy; j+=y;){
+      var sq = square(parentNode);
+      sq.style.left = i;
+      sq.style.top = y;
+    }
+  }
+
+}
